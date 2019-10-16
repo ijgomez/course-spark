47fb3e17df71e4111da6e0a9d198cdd663d215ad
Javier Izquierdo Gómez <jizquierdo@indra.es>
2019-10-16 08:25:57 +0200

    rename pom.xml

--- workspace/compile-all.cmd
+++ workspace/compile-all.cmd
@@ -10,6 +10,18 @@ if not "%ERRORLEVEL%" == "0" ( goto fin )
 call :compile ..\spark-exercise-2
 if not "%ERRORLEVEL%" == "0" ( goto fin ) 
 
+call :compile ..\spark-exercise-3
+if not "%ERRORLEVEL%" == "0" ( goto fin ) 
+
+call :compile ..\spark-exercise-4
+if not "%ERRORLEVEL%" == "0" ( goto fin ) 
+
+call :compile ..\spark-exercise-5
+if not "%ERRORLEVEL%" == "0" ( goto fin ) 
+
+call :compile ..\spark-exercise-6
+if not "%ERRORLEVEL%" == "0" ( goto fin ) 
+
 goto fin
 
 :: a function to write to a log file and write to stdout
--- workspace/run-exercise-2.cmd
+++ workspace/run-exercise-2.cmd
@@ -1,2 +1,2 @@
 
-spark-submit.cmd --master local[*] --class org.course.spark.exercise.BasicActions ..\spark-exercise-2\target\spark-exercise-2-2.11-1.0.jar
+spark-submit.cmd --master local[*] --class org.course.spark.exercise.BasicActions ..\spark-exercise-2\target\spark-core-exercise-2.11-2.0.jar
