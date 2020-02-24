62e01cd0573e04793657ca2f7fb80c28ab5b983a
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2020-02-24 18:13:56 +0100

    volumes

--- spark-environment.cmd
+++ spark-environment.cmd
@@ -1,5 +1,5 @@
 
-set JAVA_HOME=D:\Projects\personal\software\jdk1.8.0_201
+set JAVA_HOME=D:\Projects\Personal\software\Java\jdk1.8.0_231
 
 set MAVEN_HOME=D:\Program Files\Apache Software Foundation\apache-maven-3.6.0
 
@@ -7,6 +7,6 @@ set M2_REPO=D:\Projects\repository\m2_repo
 
 set MAVEN_OPTS=-Xmx512m -Dmaven.test.skip=true
 
-set HADOOP_HOME=D:\Projects\personal\software\hadoop-2.7.7
+set HADOOP_HOME=D:\Projects\personal\software\hadoop
 
 set SPARK_HOME=D:\Projects\personal\software\spark-2.4.4-bin-hadoop2.7
